468f37bc2bad65c559f670226830f6f1e4ab5481
Pavan Prakash <pavan.prakash.contractor@move.com>
2019-07-14 18:52:50 +0530

    Configure gatsby

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+'use strict';
+
+require('prismjs/themes/prism.css');
---
+++ gatsby-config.js
@@ -0,0 +1,86 @@
+'use strict';
+
+module.exports = {
+  siteMetadata: {
+    title: 'Pavan Prakash',
+    description: "Pavan's blog",
+    siteUrl: 'https://pavanprakash.now.sh',
+    author: {
+      name: 'Pavan Prakash',
+      url: 'https://twitter.com/pavanprakashcs',
+      email: 'pavanprakash21@gmail.com',
+    },
+    social: {
+      twitter: 'https://twitter.com/pavanprakashcs',
+      github: 'https://github.com/draconiandev',
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: `posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1280,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://jeffrafter.com`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jeff Rafter`,
+        short_name: `jeffrafter.com`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/logo.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-typescript`,
+    `gatsby-transformer-sharp`,
+  ],
+};
